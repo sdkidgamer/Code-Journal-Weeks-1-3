553c0d55ea5b95232619586e0a6e8b821d4f2639
sdkidgamer <sdenzof@gmail.com>
2026-01-08 13:37:58 -0800

    This is for session 2
    Let me know if you are unable to access this file.

---
+++ Session_2_prompt.py
@@ -0,0 +1,11 @@
+xfloat = float(50.000) #initialzing the floats
+yfloat = float(12.2)
+
+xint = int(12) #initialzing the integers
+yint = int(3)
+
+sumfloat = xfloat + yfloat #the equation for the floats
+differenceint = xint - yint # the equation for the integers
+
+print("here is the sum of 2 floats:\n50.000 + 12.2 =", sumfloat, "It is a ", type(sumfloat), "\n") #print statement for the floats
+print("here is the difference of 2 integers:\n12 + 3 =", "It is a ", differenceint, type(differenceint),) #print statement for the integers.
